2114f5ef3fe02be6ebfb3a6adc18ed8f66c92884
quynh <bui00047@algonquinlive.com>
2018-01-29 15:41:35 -0500

    Commit completed

---
+++ app/src/main/java/com/example/quynh/cst2335_lab/ListItemsActivity.java
@@ -0,0 +1,13 @@
+package com.example.quynh.cst2335_lab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
---
+++ app/src/main/java/com/example/quynh/cst2335_lab/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.quynh.cst2335_lab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
