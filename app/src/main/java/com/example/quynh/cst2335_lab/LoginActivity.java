95dace9c871fa3105f85df15fc852afff2de0706
quynh <bui00047@algonquinlive.com>
2018-02-01 18:16:29 -0500

    finished

--- app/src/main/java/com/example/quynh/cst2335_lab/LoginActivity.java
+++ app/src/main/java/com/example/quynh/cst2335_lab/LoginActivity.java
@@ -1,18 +1,46 @@
 package com.example.quynh.cst2335_lab;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
     protected static final String ACTIVITY_NAME = "StartActivity";
+    SharedPreferences prefs;
+    private Button button;
+    private EditText e;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        button=findViewById(R.id.LoginButton);
+        e=findViewById(R.id.email);
+        prefs = getSharedPreferences("user_data",MODE_PRIVATE);
+        e.setText(prefs.getString("DefaultEmail","email@domain.com"));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email_login = e.getText().toString().trim();
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("DefaultEmail", Email_login);
+                editor.commit();
 
-    Log.i(ACTIVITY_NAME, "In onCreate()");
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
 }
     @Override
     protected void onResume(){
