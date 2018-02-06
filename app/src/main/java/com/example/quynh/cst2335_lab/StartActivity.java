6820484fb8050c29036b62dc9e52b94852974473
quynh <bui00047@algonquinlive.com>
2018-02-05 23:08:47 -0500

    finished

--- app/src/main/java/com/example/quynh/cst2335_lab/StartActivity.java
+++ app/src/main/java/com/example/quynh/cst2335_lab/StartActivity.java
@@ -1,17 +1,55 @@
 package com.example.quynh.cst2335_lab;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class StartActivity extends Activity {
     protected static final String ACTIVITY_NAME = "StartActivity";
+    private Button button;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         Log.i(ACTIVITY_NAME, "In onCreate()");
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+
+
+                int REQUEST_CODE = 50;
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.i(ACTIVITY_NAME, "User clicked Start Chat");
+                Intent intent = new Intent(StartActivity.this, ChatWindow.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == 50) {
+            String messagePassed = data.getStringExtra("Response")+" ListItemsActivity passed: My information to share";
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+            Toast.makeText(this,messagePassed,
+                    Toast.LENGTH_LONG).show();
+        }
     }
     @Override
     protected void onResume(){
